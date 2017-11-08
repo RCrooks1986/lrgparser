dfdec3ab50d42ae1a8fd4fe6c3ce7342559fa163
bioinfochicks <christine.hicks@sch.nhs.uk>
2017-11-08 16:17:47 +0000

    Managed to load xml element tree in lrgparser.py

--- lrgparser.py
+++ lrgparser.py
@@ -1,2 +1,18 @@
-#LRG parser file, edit this as part of the project
-print('hello world')
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import xml.etree.ElementTree as ET
+
+lrg = ET.parse('C:\\Users\chris\Desktop\lrg_parser\LRG_214.xml')
+root = lrg.getroot()
+print(lrg)
+print(root)
+
+for child in root:
+    print('hello')
+    print(child.tag, child.attrib)
+    
+
