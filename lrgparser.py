db5f5c704252d73aca41274514d33e3105a84ea4
Richard Crooks <email@rocrooks.co.uk>
2017-11-09 17:04:05 +0000

    Extracting sequences from LRG

--- lrgparser.py
+++ lrgparser.py
@@ -108,6 +108,17 @@ transcripts = {}
 for m in fixed_annotation_subroot:
     #Transcript number is defined
     
+    # Sequence sub root
+    sequence_subroot = m.iter('sequence')
+    loopcount = 0        
+    
+    # Lift Genomic sequence from Genomic subroot
+    for n in sequence_subroot:
+        if loopcount == 0:
+            genomicsequence = n.text
+        
+        loopcount = loopcount+1
+    
     # Subroot of transcripts
     transcript_subroot = m.iter('transcript')
     for n in transcript_subroot:
@@ -151,6 +162,35 @@ for m in fixed_annotation_subroot:
             # Add coordinates to that exon
             transcript[exonnumber] = exon
         
+        # Subroot of cDNA
+        cdna_subroot = n.iter('cdna')
+        for o in cdna_subroot:
+            # Subroot of sequence
+            sequence_subroot = o.iter('sequence')
+            
+            # Lift cDNA from cDNA subroot
+            for p in sequence_subroot:
+                cdna = p.text
+                
+                transcript['cDNA'] = cdna
+
+        # Subroot of cDNA
+        coding_region_subroot = n.iter('coding_region')
+        for o in coding_region_subroot:
+            # Subroot of coding region
+            translation_subroot = o.iter('translation')
+            
+            # Subroot of translation
+            for p in translation_subroot:
+                # Subroot of sequence
+                sequence_subroot = p.iter('sequence')
+                
+                # Lift protein sequence from coding region subroot
+                for q in sequence_subroot:
+                    protein = q.text
+                    
+                    transcript['protein'] = protein
+            
         # Add transcript to the transcripts dictionary
         transcripts[transcriptname] = transcript
 
