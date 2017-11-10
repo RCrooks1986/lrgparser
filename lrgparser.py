ea329d6a7317237e182b3ad75e92772db68c36d9
Richard Crooks <email@rocrooks.co.uk>
2017-11-10 13:21:17 +0000

    Editing build information output HTML function

--- lrgparser.py
+++ lrgparser.py
@@ -23,50 +23,6 @@ lrgtext = data.decode('utf-8') # a `str`; this step can't be used if data is bin
 # Extract string as an element tree
 root = ET.fromstring(lrgtext)
 
-# Takes a subroot of annotation_set tags
-annotation_set_subroot = root.iter('annotation_set')
-
-# Finds only annotation sets where type="lrg"
-for m in annotation_set_subroot:
-    if m.attrib['type'] == "lrg":
-        # Send to mapping subroot
-        mapping_set_subroot = m.iter('mapping')
-
-# create dictionary to hold attributes from both builds
-dict = {}
-
-# Print mapping tags, text and attrib
-for m in mapping_set_subroot:
-    #Retrieve coord_system and remove .p
-    coord_system = m.attrib['coord_system']
-    coord_system = coord_system.split('.')[0]
-
-    # Takes a subroot of mapping_span tags
-    mapping_span_subroot = m.iter('mapping_span')
-
-    for n in mapping_span_subroot:
-        span_lrg_start = n.attrib['lrg_start']
-        span_lrg_end = n.attrib['lrg_end']
-        span_other_start = n.attrib['other_start']
-        span_other_end = n.attrib['other_end']
-        
-        # Create temp current build dictionary
-        d1 = {}
-            
-        # Populate current build dictionary
-        d1['span_lrg_start'] = span_lrg_start
-        d1['span_lrg_end'] = span_lrg_end
-        d1['span_other_start'] = span_other_start
-        d1['span_other_end'] = span_other_end
-        d1['mapping_other_start'] = m.attrib['other_start']
-        d1['mapping_other_end'] = m.attrib['other_end']
-        # Add to final dict, using the build as the key
-    
-        # Name sub dictionary as coordinate system
-        dict[coord_system] = d1
-
-print(dict)
-
 def check_build_length(dict):
     '''
     Method to check the length within each build, returns a bool
@@ -200,18 +156,6 @@ def get_build_information(root):
 # Takes a subroot of fixed annotation tags
 fixed_annotation_subroot = root.iter('fixed_annotation')
 
-# Create transcripts dictionary
-transcript = {}
-
-# test check_build_length - should print'true' to screen
-check_37 = check_build_length(dict['GRCh37'])
-check_38 = check_build_length(dict['GRCh38'])
-
-
-print(check_37)
-print(check_38)
-print('###############')
-
 # test compare_build_positions
 pos = compare_build_positions(dict)
 
@@ -228,28 +172,6 @@ print(output_comparison)
 position = compare_build_positions(dict) ## check start positions for each build
     #shift = find_position_shift(dict)
 length_diff = abs(find_length_difference(dict))
-
-html_str = "<h1>Comparison of builds </h1>"
-html_str = html_str + "<table border=1>"
-html_str = html_str + "<tr>"
-html_str = html_str + "<th>Build</th>"
-html_str = html_str + "<th>Start Position</th>"
-html_str = html_str + "<th>End Position</th>"
-html_str = html_str + "</tr>"
-
-#html_str = html_str + "<tr>"
-#html_str = html_str + "<td>" + position + "</td>"
-#html_str = html_str + "<td>" + position + "</td>"
-#html_str = html_str + "<td>" + position + "</td>"
-#html_str = html_str + "</tr>"
-
-html_str = html_str + "</table>"
-
-Html_file= open("test.html","w")
-
-Html_file.write(html_str)
-Html_file.close()
-
 
 ########CH additional bits
 for child in root:
@@ -290,34 +212,32 @@ def slice_genomic(seq, exon_dict):
          
     ####### generating output ########
 def display_output_text(build_dict, seq):
-    for build in build_dict: 
+    '''
+    Converts the build dictionaries to HTML tables
+    '''
+    html_str = ""
+    html_str = html_str + "<h1>Comparison of builds</h1>"
+    html_str = html_str + "<table border=1>"
+    html_str = html_str + "<tr>"
+    html_str = html_str + "<th>Build</th>"
+    html_str = html_str + "<th>Start Position</th>"
+    html_str = html_str + "<th>End Position</th>"
+    html_str = html_str + "</tr>"
+    
+    # Add row to table
+    for build in build_dict:
         lrg_start = build_dict[build]['span_lrg_start']
         lrg_end = build_dict[build]['span_lrg_end']
         other_start = build_dict[build]['span_other_start']
         other_end = build_dict[build]['span_other_end']
         strand = build_dict[build]['strand']
-        print(lrg_start, lrg_end, other_start, other_end, strand)
-
-
-    html_str = """
-    <h1>Comparison of builds </h1>
+        html_str = html_str + "<tr>"
+        html_str = html_str + "<td>" + build + "</td>"
+        html_str = html_str + "<td>" + other_start + "</td>"
+        html_str = html_str + "<td>" + other_end + "</td>"
+        html_str = html_str + "</tr>"
     
-    <table border=1>
-         <tr>
-         <th>Build</th>
-           <th>Start Position</th>
-           <th>End Position</th>
-         </tr>
-         <indent>
-           <tr>
-             <td><%= build %></td>
-             <td><%= other_start %></td>
-             <td><%= other_end %></td>
-           </tr>
-         </indent>
-    </table>
-    <p>{{ seq }}</p>
-    """
+    html_str = html_str + "</table>"
     
     Html_file= open("test.html","w")
     
